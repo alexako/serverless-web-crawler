5d20ad56e60995f410cd8c2b4903eac49beea4be
kludgebot <akosialexpo@gmail.com>
2024-01-30 21:14:05 -0700

    add: Crawler functionality

--- lambda/lambda_handler.py
+++ lambda/lambda_handler.py
@@ -1,10 +1,25 @@
 import boto3
+import json
+import urllib3
 
-def lambda_handler(event, context):
+
+def handler(event, context):
     print(event)
+    payload = json.loads(event["Records"][0]["body"])
+    targetURL = payload["url"]
 
-    if event:
-        return {
-            'statusCode': 200,
-            'body': 'Website crawled successfully'
-        }
+    links = findLinks(targetURL)
+
+    return {
+        'statusCode': 200,
+        'body': links
+    }
+
+
+def findLinks(link: str) -> list[str]:
+    session = urllib3.PoolManager()
+    r = session.request("GET", link)
+    print("r:", r)
+    print("r.data:", r.data)
+    return []
+
